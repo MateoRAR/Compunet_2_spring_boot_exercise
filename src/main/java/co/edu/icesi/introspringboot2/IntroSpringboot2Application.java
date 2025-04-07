aba33a6410d820f68a6d06885b072b9de561b72f
MateoRAR <mateorubio528@gmail.com>
2025-04-07 14:29:07 -0500

    Remove auto run IntroSpringboot2Application.java

--- src/main/java/co/edu/icesi/introspringboot2/IntroSpringboot2Application.java
+++ src/main/java/co/edu/icesi/introspringboot2/IntroSpringboot2Application.java
@@ -2,9 +2,10 @@ package co.edu.icesi.introspringboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IntroSpringboot2Application {
+public class IntroSpringboot2Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(IntroSpringboot2Application.class, args);
